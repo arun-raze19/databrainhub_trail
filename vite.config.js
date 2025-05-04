f67f625ffdddbb7bf0b32bc2fe39d600c6b8506b
arun-raze19 <arun.arunkumarmobile19@gmail.com>
2025-05-04 17:39:57 +0530

    .
    .

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       external: ['engine.io-client'],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
         },
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
       },
     },
     chunkSizeWarningLimit: 1000,
