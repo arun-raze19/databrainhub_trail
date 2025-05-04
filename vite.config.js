7f25cda5dc9086cd752298395808c26e8d564088
arun-raze19 <arun.arunkumarmobile19@gmail.com>
2025-05-04 17:31:38 +0530

    .
    .

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: ['engine.io-client'],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
     },
+    chunkSizeWarningLimit: 1000,
   },
 });
 
